144eb0eddaab08bfef52f0c4298633f48e6ec293
Johan Swetzén <johan.swetzen@cybercom.com>
2017-09-10 21:51:05 +0200

    Fix ShellCheck errors and add Code Climate

--- update_entrydns.sh
+++ update_entrydns.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+# vim: ts=2:sw=2
 
 [ -z "${AUTH_TOKEN}" ] && { echo "=> AUTH_TOKEN cannot be empty" && exit 1; }
 
-IPADDRESS=`curl -s icanhazip.com`
-if [[ "${IPADDRESS}" != "$(cat /current_ip)" ]]; then
+IPADDRESS=$(curl -s icanhazip.com)
+if [ "${IPADDRESS}" != "$(cat /current_ip)" ]; then
   echo "IP does not match, updating DNS"
-  echo ${IPADDRESS} >|/current_ip
+  echo "${IPADDRESS}" >|/current_ip
   curl -k -X PUT -d "ip=${IPADDRESS}" "https://entrydns.net/records/modify/${AUTH_TOKEN}"
 else
   echo "DNS record is up to date"

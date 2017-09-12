0ae9080dbacddc3333949884b7dc15e7dfa78632
Johan Swetzén <jswetzen+github@gmail.com>
2017-09-12 20:45:12 +0200

    Support multiple tokens (#3)
    With thanks to @mach-kernel

--- update_entrydns.sh
+++ update_entrydns.sh
@@ -2,11 +2,15 @@
 
 [ -z "${AUTH_TOKEN}" ] && { echo "=> AUTH_TOKEN cannot be empty" && exit 1; }
 
-IPADDRESS=`curl -s icanhazip.com`
-if [[ "${IPADDRESS}" != "$(cat /current_ip)" ]]; then
+IPADDRESS=$(curl -s icanhazip.com)
+if [ "${IPADDRESS}" != "$(cat /current_ip)" ]; then
   echo "IP does not match, updating DNS"
-  echo ${IPADDRESS} >|/current_ip
-  curl -k -X PUT -d "ip=${IPADDRESS}" "https://entrydns.net/records/modify/${AUTH_TOKEN}"
+  echo "${IPADDRESS}" >|/current_ip
+  IFS=","
+  for token in ${AUTH_TOKEN}; do
+    echo "Updating ${token}"
+    curl -k -X PUT -d "ip=${IPADDRESS}" "https://entrydns.net/records/modify/${token}"
+  done
 else
   echo "DNS record is up to date"
 fi
